98efeddd57c2b15c6bd38fb1c48675d1c64030a6
Edwin Firacative <edwin.firacative@outlook.com>
2025-05-20 16:41:10 -0500

    Inpunt output functions

---
+++ src/app/product-detail/product-detail.component.ts
@@ -0,0 +1,17 @@
+import { Component, input, output } from '@angular/core';
+import { Product } from '../product';
+
+@Component({
+  selector: 'app-product-detail',
+  imports: [],
+  templateUrl: './product-detail.component.html',
+  styleUrl: './product-detail.component.css',
+})
+export class ProductDetailComponent {
+  product = input<Product>();
+  added = output<Product>();
+
+  addToCart() {
+    this.added.emit(this.product()!);
+  }
+}
