45efdeadc2122ae2ee9167ad7cad420bb6d3c64d
Edwin Firacative <edwin.firacative@outlook.com>
2025-05-23 16:31:42 -0500

    services chap 5

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -1,55 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
 import { Product } from '../product';
 import { ProductDetailComponent } from '../product-detail/product-detail.component';
 import { SortPipe } from '../sort.pipe';
-import { NumericDirective } from '../numeric.directive';
+import { ProductsService } from '../products.service';
+import { FavoritesComponent } from '../favorites/favorites.component';
+import { ProductViewComponent } from '../product-view/product-view.component';
 
 @Component({
   selector: 'app-product-list',
-  imports: [ProductDetailComponent, SortPipe],
+  imports: [ProductDetailComponent, SortPipe, FavoritesComponent, ProductViewComponent],
   templateUrl: './product-list.component.html',
   styleUrl: './product-list.component.css',
+  providers:[ProductsService]
 })
-export class ProductListComponent {
-  products: Product[] = [{ 
-      id: 1,
-      title: 'Keyboard',
-      price: 100,
-      categories: {
-        1: 'Computing',
-        2: 'Peripherals'
-      }
-    },
-    {
-      id: 2,
-      title: 'Microphone',
-      price: 35,
-      categories: { 3: 'Multimedia' }
-    },
-    {
-      id: 3,
-      title: 'Web camera',
-      price: 79,
-      categories: {
-        1: 'Computing',
-        3: 'Multimedia'
-      }
-    },
-    {
-      id: 4,
-      title: 'Tablet',
-      price: 500,
-      categories: { 4: 'Entertainment' }
-    }
-  ];
+export class ProductListComponent implements OnInit {
+  products: Product[] = [];
+
+  // constructor(private readonly productService: ProductsService) {}
+  private productService = inject(ProductsService);
+
+  ngOnInit(): void {
+    this.products = this.productService.getProducts();
+  }
+
   selectedProduct: Product | undefined = this.products[0];
-  messaje: string = "Hola"
+  messaje: string = 'Hola';
   onAdded(product: Product) {
     alert(`${product.title} added to the cart!`);
   }
 
   myFunction() {
-    this.messaje = "Edwin Firacative"
+    this.messaje = 'Edwin Firacative';
   }
 }
 
