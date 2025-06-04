44fff8246a6a36d95979229d1aca74ebeb367f54
Edwin Firacative <edwin.firacative@outlook.com>
2025-06-04 15:33:53 -0500

    RxJS first

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,60 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ProductListComponent } from './product-list/product-list.component';
 import { CopyrightDirective } from './copyright.directive';
 import { NumericDirective } from './numeric.directive';
+import { APP_SETTINGS, appSettings } from './app.settings';
+import { Observable } from 'rxjs';
+import { KeyLoggerComponent } from './key-logger/key-logger.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ProductListComponent, CopyrightDirective, NumericDirective],
+  imports: [
+    RouterOutlet,
+    ProductListComponent,
+    CopyrightDirective,
+    NumericDirective,
+    KeyLoggerComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
+  providers: [{ provide: APP_SETTINGS, useValue: appSettings }],
 })
 export class AppComponent {
-  title = 'my-app';
+  constructor() {
+    // this.onComplete().then(this.setTitle);
+    this.title$.subscribe(this.setTitle);
+    // this.changeTitle(this.setTitle);
+  }
+
+   title = 'my-app';
+
+  title$ = new Observable((observer) => {
+    setInterval(() => {
+      observer.next(2);
+    }, 2000);
+  });
+
+  settings = inject(APP_SETTINGS);
+
+  private setTitle = () => {
+    const timestamp = new Date();
+    this.title = `${this.settings.title} (${timestamp})`;
+
+    // this.title = this.settings.title;
+  };
+
+  /* private onComplete() {
+    return new Promise<void>((resolve) => {
+      setInterval(() => {
+        resolve();
+      }, 2000);
+    });
+  } */
+
+  /* private changeTitle(callback: Function) {
+    setTimeout(() => {
+      callback();
+    }, 2000);
+  } */
 }
